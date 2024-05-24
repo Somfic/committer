bc0c918fa35f43897f596c4655fd3633f8fb71ae
Lucas <lucas@somfic.dev>
2024-05-24 04:25:24 +0200

    ✨ (cli): Added editor for commit message

--- src/main.rs
+++ src/main.rs
@@ -9,11 +9,11 @@ use serde::Deserialize;
 
 #[derive(Deserialize)]
 struct Emoji {
-    emoji: String,
-    entity: String,
-    code: String,
-    description: String,
-    name: String,
+    pub emoji: String,
+    pub entity: String,
+    pub code: String,
+    pub description: String,
+    pub name: String,
 }
 
 impl Display for Emoji {
@@ -25,19 +25,65 @@ impl Display for Emoji {
 fn main() -> anyhow::Result<()> {
     let emojis: Vec<Emoji> = serde_json::from_str(include_str!("emojis.json")).unwrap();
 
+    let scope_regex = regex::Regex::new(r"\s\((\w+)\):")?;
+
+    let scopes: HashSet<String> =
+        execute_cmd("git --no-pager log --decorate=short --pretty=oneline")?
+            .lines()
+            .filter_map(|line| scope_regex.captures(line))
+            .filter_map(|capture| capture.get(1))
+            .map(|m| m.as_str().to_string())
+            .collect();
+
     let intention = inquire::Select::new("Intention:", emojis)
         .with_help_message("What is intention behind the commit?")
         .prompt()?;
 
     // Scan previous commits to find out all the scopes
-    let commit_scope_completer = CommitScopeCompleter::new()?;
+    let commit_scope_completer = CommitScopeCompleter::new(scopes.clone());
+
+    let subject = inquire::Text::new("Subject:")
+        .with_help_message("What is the subject of the commit?")
+        .with_placeholder(&intention.description)
+        .with_validator(NonEmptyValidator)
+        .prompt()?;
 
     let scope = inquire::Text::new("Scope:")
         .with_help_message("What is the scope of the commit?")
         .with_autocomplete(commit_scope_completer)
+        .with_default(scopes.clone().iter().last().unwrap_or(&"".to_string()))
+        .prompt_skippable()?
+        .map(|s| format!("({}): ", s))
+        .unwrap_or_default();
+
+    let message = &format!("{} {}{}", intention.emoji, scope, subject);
+
+    let message = inquire::Editor::new(&message)
+        .with_help_message("What is the body of the commit?")
+        .with_predefined_text(message)
         .prompt()?;
 
+    let result = execute_cmd(&format!("git commit -m '{}", message))?;
+
     Ok(())
+}
+
+#[derive(Clone, Default)]
+pub struct NonEmptyValidator;
+
+impl inquire::validator::StringValidator for NonEmptyValidator {
+    fn validate(
+        &self,
+        input: &str,
+    ) -> Result<inquire::validator::Validation, inquire::CustomUserError> {
+        if input.is_empty() {
+            Ok(inquire::validator::Validation::Invalid(
+                anyhow!("Input cannot be empty").into(),
+            ))
+        } else {
+            Ok(inquire::validator::Validation::Valid)
+        }
+    }
 }
 
 #[derive(Clone, Default)]
@@ -46,17 +92,8 @@ pub struct CommitScopeCompleter {
 }
 
 impl CommitScopeCompleter {
-    pub fn new() -> anyhow::Result<Self> {
-        let scopes = execute_cmd("git --no-pager log --decorate=short --pretty=oneline")?
-            .lines()
-            .filter_map(|line| line.split_once(":"))
-            .map(|(scope, _)| scope.trim())
-            .map(|scope| scope.to_string())
-            .collect::<std::collections::HashSet<_>>()
-            .into_iter()
-            .collect();
-
-        Ok(Self { scopes })
+    pub fn new(scopes: HashSet<String>) -> Self {
+        Self { scopes }
     }
 }
 
@@ -92,6 +129,7 @@ fn execute_cmd(cmd: &str) -> anyhow::Result<String> {
     if output.status.success() {
         Ok(std::str::from_utf8(&output.stdout)?.into())
     } else {
-        Ok(String::new())
+        let stderr = std::str::from_utf8(&output.stderr)?.to_string();
+        Err(anyhow!(stderr))
     }
 }
