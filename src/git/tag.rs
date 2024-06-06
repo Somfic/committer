f3727322388440936b0740d696098b1d7fa18499
Lucas <lucas@somfic.dev>
2024-06-06 05:33:15 +0200

    🐛 Fetch all tags before trying to find the latest one
    semver: patch

--- src/git/tag.rs
+++ src/git/tag.rs
@@ -1,6 +1,8 @@
 use crate::cmd::execute;
 
 pub fn latest() -> anyhow::Result<String> {
+    execute("git", vec!["fetch", "--tags"])?;
+
     let result = execute("git", vec!["describe", "--tags", "--abbrev=0"])?;
 
     Ok(result.trim().to_string())
