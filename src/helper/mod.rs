975e9b446b8c2ae51a55146c31e4949db523c605
Lucas <lucas@somfic.dev>
2024-06-06 14:26:52 +0200

    🐛 Revert writing env to files
    semver: patch

--- src/helper/mod.rs
+++ src/helper/mod.rs
@@ -1,4 +1,4 @@
-// use std::io::Write;
+use std::io::Write;
 
 use anyhow::Ok;
 
@@ -73,10 +73,9 @@ pub fn calculate_new_tag_based_on_commits() -> anyhow::Result<Option<semver::Ver
 }
 
 pub fn set_github_env_var(name: &str, value: &str) -> anyhow::Result<()> {
-    // std::fs::create_dir_all(".env")?;
-    // let mut file = std::fs::File::create(format!(".env/{}", name))?;
-    // file.write_all(value.as_bytes())?;
-    std::env::set_var(name, value);
+    std::fs::create_dir_all(".env")?;
+    let mut file = std::fs::File::create(format!(".env/{}", name))?;
+    file.write_all(value.as_bytes())?;
 
     Ok(())
 }
